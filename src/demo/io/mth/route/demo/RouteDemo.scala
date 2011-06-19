012b63089579a08950c88b2eebcff12c227d84ba
Mark Hibberd <mark@hibberd.id.au>
2011-06-19 23:23:24 +1000

    Building out the matchers

---
+++ src/demo/io/mth/route/demo/RouteDemo.scala
@@ -0,0 +1,52 @@
+package io.mth.route.demo
+
+import io.mth.route._
+import Path._
+import Request._
+
+object RouteDemo {
+  def main(args: Array[String]) {
+  }
+/*
+  def route =
+    ("foo" </> "bar").route(
+      Get.route({
+        "get[" + _ + "]"
+        html.route()
+      
+      })
+      Get => "get[" + _ + "]
+    )
+
+    ("foo" </> "bar").resource(Get.route(a) | Post.route(b))
+
+    ("foo" </> "bar").resource(Get.route(a) | Post.route(b))
+    *
+    ("foo" </> "bar").resource(
+      contentType r
+      (Get.route(a) | Post.route(b))
+    )
+    *
+    ("foo" </> numeric)
+
+    ("foo" </> string)
+
+    
+  
+      
+    )
+
+    
+    id(Get, "foo" </> "bar", "get[" + _ + "]") :>:
+    id(Post, "foo" </> "bar", "post[" + _ + "]") :>:
+    is(Get, "foo" </> "bar" </> "baz", "exact match") :>:
+    rule(rs => Some("special case")) :>:
+    base("base case")
+  
+   def main(args: Array[String]) = {
+    val line = request(Post, "foo" </> "bar" </> "baz")
+    val result = route(line)
+    println(result)
+  }
+*/
+}
